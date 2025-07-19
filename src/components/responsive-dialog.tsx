82088bd74c47063eead3db83971be3d08c8740b7
sabbirHossain <sabbirshnt@gmail.com>
2025-07-19 13:44:12 +0600

    10-responsive-dialog

---
+++ src/components/responsive-dialog.tsx
@@ -0,0 +1,57 @@
+import { useIsMobile } from "@/hooks/use-mobile";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+} from "./ui/drawer";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+} from "./ui/dialog";
+import { DialogTitle } from "@radix-ui/react-dialog";
+
+interface ResponsiveDialogProps {
+  title: string;
+  description: string;
+  children: React.ReactNode;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+export const ResponsiveDialog: React.FC<ResponsiveDialogProps> = ({
+  title,
+  description,
+  children,
+  open,
+  onOpenChange,
+}) => {
+  const isMobile = useIsMobile();
+  if (isMobile) {
+    return (
+      <Drawer open={open} onOpenChange={onOpenChange}>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>{title}</DrawerTitle>
+            <DrawerDescription>{description}</DrawerDescription>
+          </DrawerHeader>
+          <div className="p-4">{children}</div>
+        </DrawerContent>
+      </Drawer>
+    );
+  }
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+        <div className="p-4">{children}</div>
+      </DialogContent>
+    </Dialog>
+  );
+};
